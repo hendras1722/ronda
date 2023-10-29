1be8b98b28419af03b120fcb413d470ed41dba34
syahendra <syahendra@privyid.tech>
2023-10-29 17:57:31 +0700

    fix: fixing camera landscape and take capture camera

--- pages/camera.vue
+++ pages/camera.vue
@@ -1,38 +1,78 @@
 <template>
   <div>
     <div class="font-bold text-2xl">Camera Oi Oi Oi</div>
-    <UButton @click="handleOpen" class="mt-3 dark:text-white"
+    <UButton @click="handleOpen" class="mt-3 dark:text-white overflow-hidden"
       >Klik Disini</UButton
     >
+    <div v-if="imageSrc">
+      result:
+      <img :src="imageSrc" />
+    </div>
     <UModal
       v-model="isOpen"
       fullscreen
       class="p-0 m-0"
       :ui="{
         container:
-          'flex min-h-screen items-end sm:items-center justify-center text-center',
+          'relative flex min-h-screen items-end sm:items-center justify-center text-center',
         base: 'fixed text-left rtl:text-right overflow-hidden w-full flex flex-col',
       }"
     >
-      <div class="relative cursor-pointer z-10" v-if="!loadingVideos">
-        <div
-          @click="stopCameraStream"
-          class="absolute top-0 right-0 mt-3 mr-3 text-2xl"
-        >
-          <UIcon name="i-ion-close-circle"></UIcon>
+      <div v-if="!imageSrc">
+        <div class="relative cursor-pointer z-10" v-if="!loadingVideos">
+          <div
+            @click="stopCameraStream()"
+            class="absolute top-0 right-0 mt-3 mr-3 text-2xl"
+          >
+            <UIcon name="i-ion-close-circle"></UIcon>
+          </div>
         </div>
+        <!-- end:Button close -->
+        <div class="w-full">
+          <video id="video" class="h-full" />
+        </div>
+        <!-- Button Capture -->
+        <div
+          class="bg-white backdrop-blur-sm bg-opacity-10 w-20 h-20 rounded-full absolute bottom-0 mb-5 left-0 right-0 mx-auto overflow-hidden"
+          @click="handleCapture"
+        >
+          <div
+            class="bg-white w-14 h-14 rounded-full z-10 absolute left-0 right-0 mx-auto top-0 bottom-0 my-auto"
+          ></div>
+        </div>
+        <!--  button close -->
       </div>
-      <div class="w-full">
-        <video id="video" class="h-full" />
+      <div v-else>
+        <img :src="imageSrc" />
+
+        <div
+          class="absolute bottom-0 mb-3 left-0 right-0 mx-auto flex justify-center"
+        >
+          <UButton
+            :ui="{ base: 'text-center text-white' }"
+            class="rounded-md overflow-hidden"
+            variant="outline"
+            color="green"
+            @click="isOpen = false"
+            >Save</UButton
+          >
+        </div>
       </div>
     </UModal>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { useWindowSize } from '@vueuse/core'
+
 const isOpen = ref(false)
 const loadingVideos = ref(false)
+const imageSrc = ref('')
 
+const { width } = useWindowSize()
+/*@@@@@@@
+  Init Camera
+@@@@@@*/
 async function cameraStream() {
   loadingVideos.value = true
   const ratioCalc: string[] = aspectRatio(
@@ -40,36 +80,34 @@ async function cameraStream() {
     window.innerHeight
   ).split(':')
 
-  // const userAgentData = (navigator as any).userAgentData
+  const userAgentData = (navigator as any).userAgentData
 
   let aspectRatioCalc = undefined
-  // if (userAgentData && userAgentData.mobile) {
-  //   alert('mobileuseragent')
-  //   aspectRatioCalc = Number(ratioCalc[1]) / Number(ratioCalc[0])
-  // } else {
-  //   if (navigator.maxTouchPoints === 1) {
-  //     aspectRatioCalc = Number(ratioCalc[0]) / Number(ratioCalc[1])
-  //   } else if (
-  //     /Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile|Kindle|NetFront|Silk-Accelerated|(hpw|web)OS|Fennec|Minimo|Opera M(obi|ini)|Blazer|Dolfin|Dolphin|Skyfire|Zune/.test(
-  //       navigator.userAgent
-  //     )
-  //   ) {
-  //     alert('regex')
-  //     aspectRatioCalc = Number(ratioCalc[1]) / Number(ratioCalc[0])
-  //   }
-  // }
-
-  function isMobile() {
-    try {
-      document.createEvent('TouchEvent')
-      return true
-    } catch (e) {
-      return false
+  if (userAgentData && userAgentData.mobile) {
+    aspectRatioCalc = Number(ratioCalc[1]) / Number(ratioCalc[0])
+  } else {
+    if (navigator.maxTouchPoints === 1) {
+      aspectRatioCalc = Number(ratioCalc[0]) / Number(ratioCalc[1])
+    } else if (
+      /Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile|Kindle|NetFront|Silk-Accelerated|(hpw|web)OS|Fennec|Minimo|Opera M(obi|ini)|Blazer|Dolfin|Dolphin|Skyfire|Zune/.test(
+        navigator.userAgent
+      )
+    ) {
+      aspectRatioCalc = Number(ratioCalc[1]) / Number(ratioCalc[0])
     }
   }
-  if (isMobile()) {
-    aspectRatioCalc = Number(ratioCalc[1]) / Number(ratioCalc[0])
-  }
+
+  // function isMobile() {
+  //   try {
+  //     document.createEvent('TouchEvent')
+  //     return true
+  //   } catch (e) {
+  //     return false
+  //   }
+  // }
+  // if (isMobile()) {
+  //   aspectRatioCalc = Number(ratioCalc[1]) / Number(ratioCalc[0])
+  // }
 
   const arr: any = []
   const mediaDevices = navigator.mediaDevices as any
@@ -123,6 +161,11 @@ async function cameraStream() {
   return res
 }
 
+/**
+ *
+ * @Calculate aspect ratio and GCD
+ *
+ */
 function gcd(a: number, b: number): number {
   return b ? gcd(b, a % b) : a
 }
@@ -133,11 +176,17 @@ function aspectRatio(width: number, height: number): string {
 }
 
 async function handleOpen() {
+  imageSrc.value = ''
   isOpen.value = true
   cameraStream()
 }
+/**
+ *
+ * Stop Camera
+ *
+ */
 
-function stopCameraStream() {
+function stopCameraStream(e?: string) {
   const videos = document.getElementById('video') as HTMLVideoElement
   if (!videos) return
   const tracks = (videos?.srcObject as any)?.getTracks()
@@ -147,8 +196,34 @@ function stopCameraStream() {
     track.stop()
   })
 
+  if (e) return
   isOpen.value = false
 }
+
+function handleCapture() {
+  if (loadingVideos.value) return
+
+  const video = document.getElementById('video') as HTMLVideoElement
+  // Create a canvas element and draw the video frame onto it
+  const canvas = document.createElement('canvas')
+  const ctx = canvas.getContext('2d')
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+  if (!ctx) return
+  ctx.drawImage(video, 0, 0, canvas.width, canvas.height)
+
+  // Convert the canvas image data to a data URL and create an image element
+  imageSrc.value = canvas.toDataURL('image/png')
+  stopCameraStream('capture')
+}
+
+watch(width, () => {
+  stopCameraStream('resize')
+
+  setTimeout(() => {
+    cameraStream()
+  }, 1000)
+})
 </script>
 
 <style scoped lang="scss">
